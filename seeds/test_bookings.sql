14660200aa66295a2ae43513438ae888224442f0
Paul Gilson <paul.gilson@makers.tech>
2024-11-18 13:45:05 +0000

    upload v1

---
+++ seeds/test_bookings.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS bookings;
+DROP SEQUENCE IF EXISTS bookings_id_seq;
+
+-- Then, we recreate them
+CREATE SEQUENCE IF NOT EXISTS bookings_id_seq;
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    datetime TIMESTAMP, -- of the booking
+    gig_id INTEGER,
+    user_id INTEGER,
+    ticket_count INTEGER,
+    constraint fk_gig foreign key(gig_id)
+      references gigs(id),
+    constraint fk_user foreign key(user_id)
+      references users(id)
+);
+
+INSERT INTO bookings (datetime, gig_id, user_id, ticket_count) VALUES ('2024-06-21 15:43', 1, 1, 1);
+INSERT INTO bookings (datetime, gig_id, user_id, ticket_count) VALUES ('2024-06-21 15:43', 4, 1, 4);
